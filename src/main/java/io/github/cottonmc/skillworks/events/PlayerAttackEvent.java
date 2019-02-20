420b9b2c78a2cd11ef272436f6b10bf70f2092e7
Meredith Espinosa <goclonefilms@gmail.com>
2019-02-19 16:24:59 -0800

    make fisticuffs not work if an enemy has armor on

--- src/main/java/io/github/cottonmc/skillworks/events/PlayerAttackEvent.java
+++ src/main/java/io/github/cottonmc/skillworks/events/PlayerAttackEvent.java
@@ -4,7 +4,10 @@ import io.github.cottonmc.skillworks.Skillworks;
 import me.elucent.earlgray.api.TraitRegistry;
 import me.elucent.earlgray.api.Traits;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 
 public class PlayerAttackEvent {
@@ -12,6 +15,9 @@ public class PlayerAttackEvent {
 	public static AttackEntityCallback onPlayerAttack = (player, world, hand, entity, hitResult) -> {
 		if (player.getStackInHand(hand).isEmpty()) {
 			if (Traits.get(player, Skillworks.FISTICUFFS).getValue() > 0) {
+				for (ItemStack stack : entity.getItemsArmor()) {
+					if (stack.getItem() instanceof ArmorItem) return ActionResult.PASS;
+				}
 				entity.damage(DamageSource.player(player), Traits.get(player, Skillworks.FISTICUFFS).getValue());
 				return ActionResult.SUCCESS;
 			}
