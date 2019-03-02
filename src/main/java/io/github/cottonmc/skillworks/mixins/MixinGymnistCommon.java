928cc36e1702fc831d1df1d7d37ef1a881e390e4
Meredith Espinosa <goclonefilms@gmail.com>
2019-03-02 03:33:45 -0800

    make it seem more like you're actually catchin arrows

--- src/main/java/io/github/cottonmc/skillworks/mixins/MixinGymnistCommon.java
+++ src/main/java/io/github/cottonmc/skillworks/mixins/MixinGymnistCommon.java
@@ -14,6 +14,7 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -29,6 +30,8 @@ public abstract class MixinGymnistCommon extends LivingEntity {
 	@Shadow @Nullable
 	public abstract ItemEntity dropItem(ItemStack stack, boolean fromSelf);
 
+	@Shadow @Final public PlayerInventory inventory;
+
 	protected MixinGymnistCommon(EntityType<? extends LivingEntity> type, World world) {
 		super(type, world);
 	}
@@ -36,7 +39,9 @@ public abstract class MixinGymnistCommon extends LivingEntity {
 	@Inject(method = "damage", at = @At("HEAD"), cancellable = true)
 	public void catchArrow(DamageSource source, float amount, CallbackInfoReturnable ci) {
 		if (source.isProjectile() && source.getSource() instanceof ArrowEntity) {
-			if (ClassManager.hasLevel(this, Skillworks.GYMNIST, 3) && world.random.nextFloat() < Skillworks.config.arrowCatchChance) {
+			if (ClassManager.hasLevel(this, Skillworks.GYMNIST, 3)
+					&& world.random.nextFloat() < Skillworks.config.arrowCatchChance
+					&& canCatchArrow()) {
 				ArrowEntity arrow = (ArrowEntity) source.getSource();
 				if (!((ArrowEffects)arrow).getEffects().isEmpty()) {
 					for (StatusEffectInstance effect : (((ArrowEffects) arrow).getEffects())) {
@@ -44,12 +49,32 @@ public abstract class MixinGymnistCommon extends LivingEntity {
 					}
 				}
 				if (arrow.isOnFire()) this.setOnFireFor(5);
-					if (!((PlayerEntity) (Object) this).inventory.insertStack(new ItemStack(Items.ARROW))) {
-						this.dropItem(new ItemStack(Items.ARROW), false);
-					}
+				ItemStack main = this.getMainHandStack();
+				ItemStack off = this.getOffHandStack();
+				if (main.isEmpty()) {
+					this.setStackInHand(Hand.MAIN, new ItemStack(Items.ARROW));
+					this.swingHand(Hand.MAIN);
+				} else if (main.getItem() == Items.ARROW && main.getAmount() < Items.ARROW.getMaxAmount()) {
+					main.addAmount(1);
+					this.swingHand(Hand.MAIN);
+				} else if (off.isEmpty()) {
+					this.setStackInHand(Hand.OFF, new ItemStack(Items.ARROW));
+					this.swingHand(Hand.OFF);
+				} else if (off.getItem() == Items.ARROW && off.getAmount() < Items.ARROW.getMaxAmount()) {
+					off.addAmount(1);
+					this.swingHand(Hand.OFF);
+				} else this.dropItem(new ItemStack(Items.ARROW), false);
+
 				ci.setReturnValue(true);
 			}
 		}
 	}
+
+	private boolean canCatchArrow() {
+		return this.getMainHandStack().isEmpty() || this.getOffHandStack().isEmpty()
+				|| (this.getMainHandStack().getItem() == Items.ARROW && this.getMainHandStack().getAmount() < Items.ARROW.getMaxAmount())
+				|| (this.getOffHandStack().getItem() == Items.ARROW && this.getOffHandStack().getAmount() < Items.ARROW.getMaxAmount());
+	}
+
 
 }
