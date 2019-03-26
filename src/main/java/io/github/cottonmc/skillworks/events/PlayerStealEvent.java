2ce8e894e92990f876c565a685cf12e6e186cb37
Meredith Espinosa <goclonefilms@gmail.com>
2019-03-25 18:18:39 -0700

    tweak pickpocketing

--- src/main/java/io/github/cottonmc/skillworks/events/PlayerStealEvent.java
+++ src/main/java/io/github/cottonmc/skillworks/events/PlayerStealEvent.java
@@ -17,35 +17,37 @@ public class PlayerStealEvent {
 		if (player.isSpectator()
 				|| !player.getStackInHand(hand).isEmpty()
 				|| !ClassManager.hasClass(player, Skillworks.THIEF)
-				|| !(entity instanceof MobEntity)) return ActionResult.PASS;
+				|| !(entity instanceof MobEntity)
+				|| world.isClient) return ActionResult.PASS;
 		MobEntity mob = (MobEntity) entity;
-		if (mob.getTarget() == null || !mob.getTarget().equals(player)) {
-			Random rand = new Random();
-			for (ItemStack stack : mob.getItemsArmor()) {
-				if (stack.isEmpty()) continue;
-				int roll = rand.nextInt(20) + 1;
-				if (roll == 1) {
-					player.attack(mob);
-					mob.setTarget(player);
-					mob.addPotionEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 1000));
-					return ActionResult.SUCCESS;
-				}
-				roll += ClassManager.getLevel(player, Skillworks.THIEF);
-				if (roll > 6) {
-					ItemStack give = stack.copy();
-					stack.subtractAmount(1);
-					player.setStackInHand(hand, give);
-					if (roll < 12) {
-						player.attack(mob);
-						mob.setTarget(player);
-					}
-					return ActionResult.SUCCESS;
-				}
+		//TODO: try to figure out a good way to require you to sneak around to pickpocket?
+		Random rand = new Random();
+		for (ItemStack stack : mob.getItemsArmor()) {
+			if (stack.isEmpty()) continue;
+			int roll = rand.nextInt(20) + 1;
+			if (roll == 1) {
+				System.out.println("Critical fail!");
+				player.attack(mob);
+				if (!player.isCreative()) mob.setTarget(player);
+				mob.addPotionEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 2000));
+				return ActionResult.SUCCESS;
 			}
-			player.attack(mob);
-			mob.setTarget(player);
-			return ActionResult.SUCCESS;
+			roll += ClassManager.getLevel(player, Skillworks.THIEF);
+			System.out.println(roll);
+			if (roll > 6) {
+				ItemStack give = stack.copy();
+				stack.subtractAmount(1);
+				player.setStackInHand(hand, give);
+				if (roll < 12) {
+					player.attack(mob);
+					if (!player.isCreative()) mob.setTarget(player);
+				}
+				return ActionResult.SUCCESS;
+			}
 		}
-		return ActionResult.PASS;
+		player.attack(mob);
+		if (!player.isCreative()) mob.setTarget(player);
+		return ActionResult.SUCCESS;
+
 	};
 }
