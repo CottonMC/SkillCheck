a9b64a7e2aa6889a18de89fedf1b24ebb0a8de65
Meredith Espinosa <goclonefilms@gmail.com>
2019-05-08 11:21:44 -0700

    Fix mixin and crochet issues, closes #2

--- src/main/java/io/github/cottonmc/skillcheck/mixins/MixinLoomContainer.java
+++ src/main/java/io/github/cottonmc/skillcheck/mixins/MixinLoomContainer.java
@@ -2,11 +2,10 @@ package io.github.cottonmc.skillcheck.mixins;
 
 import io.github.cottonmc.skillcheck.SkillCheck;
 import io.github.cottonmc.skillcheck.api.traits.ClassManager;
-import net.minecraft.client.gui.container.LoomScreen;
+import net.minecraft.container.BlockContext;
 import net.minecraft.container.LoomContainer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.text.TextComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -14,18 +13,19 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LoomScreen.class)
-public abstract class MixinWeaverScreen {
+@Mixin(LoomContainer.class)
+public abstract class MixinLoomContainer {
 	private PlayerEntity player;
 
-	@Inject(method = "<init>", at = @At("RETURN"))
-	public void construct(LoomContainer container, PlayerInventory inv, TextComponent name, CallbackInfo ci) {
+	@Inject(method = "<init>(ILnet/minecraft/entity/player/PlayerInventory;Lnet/minecraft/container/BlockContext;)V", at = @At("RETURN"))
+	public void construct(int id, PlayerInventory inv, final BlockContext ctx, CallbackInfo ci) {
 		player = inv.player;
 	}
 
-	@ModifyConstant(method = "onInventoryChanged", constant = @Constant(intValue = 6))
+	@ModifyConstant(method = "onContentChanged", constant = @Constant(intValue = 6))
 	private int changeBannerPatternLimit(int orig) {
 		if (ClassManager.hasClass(player, SkillCheck.ARTISAN)) return 16;
 		else return orig;
 	}
+
 }
